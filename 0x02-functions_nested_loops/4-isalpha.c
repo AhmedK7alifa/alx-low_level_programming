f7624aa125672e738358fdfef4ad073e7045d2df
ahmedk7alifa <ahmedk7alifa@gmail.com>
2022-10-23 01:11:34 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,26 @@
 #include "main.h"
 
 /**
- * _isalpha - checks if character is a letter
- *            both lowercase or uppercase
+ * test_isalpha - Test the _isalpha function
  *
- * @c: takes input from other functions.
- *
- * Return: 1 is c if true else 0
+ * @n: Number to pass to _isalpha function
  */
 
-int _isalpha(int c)
+void test_isalpha(int n)
 {
-	if (c >= 97 && c <= 122 &&
-			c >= 65 && c >= 90)
-		return (1);
+	int r;
+
+	r = _isalpha(n);
+	_putchar(r + '0');
+	_putchar('\n');
+}
+
+/**
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	test_isalpha('H');
 	return (0);
 }
